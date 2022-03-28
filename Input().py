480f47e874adeafee851ad93b3036236b1964099
Gamze Ağaçayak <77496608+gamzeagacayak@users.noreply.github.com>
2022-03-28 14:45:11 +0300

    Update and rename Input() to Input().py

--- Input().py
+++ Input().py
@@ -1,10 +1,11 @@
-//**TASK
-You are given a polynomial P of a single indeterminate (or variable), x.
+"""
+TASK: You are given a polynomial P of a single indeterminate (or variable), x.
 You are also given the values of x and k. Your task is to verify if P(x)=k.
 
 Constraints
 All coefficients of polynomial P are integers.
-x and y are also integers.**//
+x and y are also integers.
+"""
  
  
 a = raw_input().split()
