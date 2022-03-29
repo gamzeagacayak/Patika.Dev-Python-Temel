4c0b9adddb1ad50bedaad1115de4bd94f876e718
Gamze Ağaçayak <77496608+gamzeagacayak@users.noreply.github.com>
2022-03-29 12:22:24 +0300

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,12 @@
+"""
+Task: The provided code stub reads and integer, n, from STDIN. For all non-negative integers i<n, print i^2.
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    i = 0
+while i < n:
+    
+    print (i**2)
+    
+    i+=1
