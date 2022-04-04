8137bdac798c0e1a0434ac4534946d9a44757769
Gamze Ağaçayak <77496608+gamzeagacayak@users.noreply.github.com>
2022-04-04 16:53:33 +0300

    Create List_Comprehensions.py

---
+++ List_Comprehensions.py
@@ -0,0 +1,17 @@
+"""
+Task: You are given three integers x, y and z representing the dimensions of a cuboid along with an integer n. 
+Print a list of all possible coordinates given by (i, j, k) on a 3D grid where the sum of i + j + k is not equal to n. 
+Here, 0<=i<=x, 0<=j<=y, 0<=k<=z.
+"""
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+print ([[i,j,k] 
+    for i in range(0,x+1) 
+    for j in range(0,y+1) 
+    for k in range(0,z+1) 
+        if i + j + k != n ])
