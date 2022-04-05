7f5b7fdda8b65fb37fb40e9a777fc084edaf1c92
Gamze Ağaçayak <77496608+gamzeagacayak@users.noreply.github.com>
2022-04-05 18:28:39 +0300

    Create Arrays.py

---
+++ Arrays.py
@@ -0,0 +1,14 @@
+"""
+Task: You are given a space separated list of numbers.
+Your task is to print a reversed NumPy array with the element type float.
+"""
+
+import numpy
+
+def arrays(arr):
+    
+   return(numpy.array(arr[::-1], float))
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
